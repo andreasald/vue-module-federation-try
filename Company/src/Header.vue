7dda9572bd48dfa52988bb0c1e6c69e830919e98
andreasald <andreas.aldito.08@gmail.com>
2023-04-12 08:50:45 +0700

    init push

---
+++ Company/src/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h2>App Header</h2>
+      <p>items: {{ cartCount }}</p>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    cartCount() {
+      return this.$store.state.cartItems;
+    },
+  },
+};
+</script>
